db150e5d8c29c51a8ec2a8d45c09b8b652fbc62d
Jack Whelan <jwhe0002@student.monash.edu>
2021-07-27 00:31:42 +1000

    refactor: cleaned up code and extracted functions

--- LOC_per_release.py
+++ LOC_per_release.py
@@ -2,15 +2,15 @@ import subprocess
 import json
 import requests
 from tqdm import tqdm
+import os
 
 import secrets
 
+BASE_DIR = "./repositories"
+
 repo_name = "react"
 repo_owner = "facebook"
-
 data = {}
-
-# p1 = subprocess.run(f"git clone https://github.com/{repo_owner}/{repo_name}.git", capture_output=True, text=True, cwd="./repositories")
 
 # releases = ["v17.0.2", "v17.0.1", "v17.0.0"]
 
@@ -25,27 +25,55 @@ def get_releases():
     return releases
 
 
-releases = get_releases()
-print(len(releases))
+def check_repo(repo_name):
+    # TODO: check the directory is not empty and is a git repository
+    if os.path.exists(f"{BASE_DIR}/{repo_name}"):
+        if os.path.isdir(f"{BASE_DIR}/{repo_name}"):
+            return True
+    return False
 
 
-for release in tqdm(releases, desc="calculating LOC for each release"):
-    tag = release["tag_name"]
-    print(f"checking out tag: {tag}")
-    p2 = subprocess.run(f"git checkout {tag}", cwd=f"./repositories/{repo_name}", capture_output=True)
-    if p2.returncode == 0:
+def clone_repo(repo_name, repo_owner):
+    p1 = subprocess.run(f"git clone https://github.com/{repo_owner}/{repo_name}.git", capture_output=True, text=True,
+                        cwd=BASE_DIR)
+    if p1.returncode != 0:
+        raise SystemError(p1.stderr)
+
+
+def process_repository(repo_name, repo_owner):
+
+    # check if repository is already there
+    if check_repo(repo_name):
+        print("using cached repository")
         pass
     else:
-        print("error")
-        break
-    print(f"count LOC for tag: {tag}")
-    p3 = subprocess.run(f"cloc . --vcs=git --json", cwd=f"./repositories/{repo_name}", capture_output=True)
-    if p3.returncode == 0:
-        pass
-    else:
-        print("error")
-        break
-    data[tag] = json.loads(p3.stdout)
+        print("there must be an error")
+        exit(-1)
+        clone_repo(repo_name, repo_owner)
 
-with open('loc_per_release_for_facebook_react.json', 'w') as file:
-    file.write(json.dumps(data))
+    releases = get_releases()
+    assert len(releases) > 0, "There must be at least one release"
+
+    for release in tqdm(releases, desc="calculating LOC for each release"):
+        tag = release["tag_name"]
+        print(f"checking out tag: {tag}")
+        p2 = subprocess.run(f"git checkout {tag}", cwd=f"{BASE_DIR}/{repo_name}", capture_output=True)
+        if p2.returncode == 0:
+            pass
+        else:
+            print("error")
+            break
+        print(f"count LOC for tag: {tag}")
+        p3 = subprocess.run(f"cloc . --vcs=git --json", cwd=f"{BASE_DIR}/{repo_name}", capture_output=True)
+        if p3.returncode == 0:
+            pass
+        else:
+            print("error")
+            break
+        data[tag] = json.loads(p3.stdout)
+
+    with open(f'loc_per_release_for_{repo_owner}_{repo_name}.json', 'w') as file:
+        file.write(json.dumps(data))
+
+
+process_repository(repo_name, repo_owner)
