cc365334789595893778a36fada84b795638762c
Jack Whelan <jwhe0002@student.monash.edu>
2021-07-31 22:37:34 +1000

    feat: added test cases for the repository name testing

--- test_pipeline_draft.py
+++ test_pipeline_draft.py
@@ -6,8 +6,14 @@ class TestPipeline(unittest.TestCase):
         return
 
     def test_repository_string(self):
-        self.assertTrue(LOC_per_release.is_valid_repo_name('facebook/react'), msg="'facebook/react' is a valid name")
-        self.assertFalse(LOC_per_release.is_valid_repo_name('facebook//react'), msg="'facebook//react' is an invalid name")
+        valid = ["f/r", "facebook/react", "ABC/defg", "a-hyphen/a-hyphen", "a.dot/a.dot"]
+        invalid = ["f//r", "facebook/", "/react", "vue js/vue", "vuejs /vue", "vuejs/ vue", "vuejs/vue/"]
+
+        for repo_str in valid:
+            self.assertTrue(LOC_per_release.is_valid_repo_name(repo_str), msg=f"{repo_str} is a valid name")
+
+        for repo_str in invalid:
+            self.assertFalse(LOC_per_release.is_valid_repo_name(repo_str), msg=f"{repo_str} is an invalid name")
 
     def tearDown(self) -> None:
         pass
